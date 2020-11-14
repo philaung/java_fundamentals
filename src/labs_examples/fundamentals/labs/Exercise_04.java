f12e748204b27b9fdb1fb10fa96c199f5890199e
zuugar <phyo.naung@yahoo.com>
2020-11-14 12:03:15 -0800

    lab 4 and 5

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,29 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        boolean trueORfalse = true;
+        System.out.println("bool is : " + trueORfalse);
+
+        char vowel = 'a';
+        System.out.println("char is : " + vowel);
+
+        byte daByte = 6;
+        System.out.println("byte is : " + daByte);
+
+        short shortie = 546;
+        System.out.println("short is : " + shortie);
+
+        int inter = 298756;
+        System.out.println("int is : " + inter);
+
+        long longBoi = 999999999999999999L;
+        System.out.println("long is : " + longBoi);
+
+        float floatyPoint = 123.456789F;
+        System.out.println("float is : " + floatyPoint);
+
+        double doubleTrouble = 9.87654321123456789;
+        System.out.println("double is : " + doubleTrouble);
 
     }
 
